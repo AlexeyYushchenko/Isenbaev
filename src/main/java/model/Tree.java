0f453207b3aa3c83cf538dfa00eafd5fd9cb3c70
Aleksey Yushchenko <68127499+AlexeyYushchenko@users.noreply.github.com>
2022-08-15 00:01:38 +0300

    added description to methods

--- src/main/java/model/Tree.java
+++ src/main/java/model/Tree.java
@@ -18,8 +18,9 @@ public class Tree {
     private void initialize() {
         try {
             List<String> lines = Files.readAllLines(Path.of("input.txt"));
-            for (String line : lines) {
-                String[] split = line.split(" ");
+            int numberOfTeams = Integer.parseInt(lines.get(0));
+            for (int i = 1; i <= numberOfTeams; i++) {
+                String[] split = lines.get(i).split(" ");
                 List<Player> team = new ArrayList<>();
                 for (String name : split) {
                     Player player = getPlayer(name);
@@ -51,7 +52,8 @@ public class Tree {
     }
 
     /**
-     *
+     * Breadth-first-search algorithm helps us determine if one player is related to another.
+     * If so, we get the number of handshakes from one player to another.
      * @param s - start node;
      * @param e - end node;
      * @return length of nodes between s and e and list of these nodes.
@@ -63,8 +65,11 @@ public class Tree {
     }
 
     /**
-     * @param s - start node
-     * @return - array of PREVious nodes where prev[index of a current node] = index of the previous node.
+     * First part of the Bfs algorithm where we get an array of connections between players,
+     * so that index of the current player leads to the previous player it played with.
+     * Example: prev[index of the current player] = previous player.
+     * @param s - start node (player).
+     * @return 'prev' (previous) array - array of players where prev[current player index] = previous player.
      */
 
     public Player[] solve(Player s) {
@@ -88,6 +93,19 @@ public class Tree {
         }
         return prev;
     }
+
+    /**
+     * Second part of the Bfs algorithm where we reconstruct the path from 'e' (end) player to 's' (start) player.
+     * If relation exists, then we return number of handshakes between two players,
+     * 0 handshakes mean 's' and 'e' is the same player,
+     * 1 handshake means 's' player has played with the 'e' player in the same team.
+     * 2 handshakes mean 's' player has played with the player who played with the 'e' player in the same team, etc.
+     * "undefined" means there's no relation between two players was found.
+     * @param s 'start' player.
+     * @param e 'end' player.
+     * @param prev ('previous') array of players where prev[current player index] = previous player.
+     * @return number of handshakes between 's' player and 'e' player and the path between them if relation exists.
+     */
 
     public String reconstruct(Player s, Player e, Player[] prev) {
         if (s.equals(e)) return "0";
